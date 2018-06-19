459634893bb2c54b23f45c28bd4a6b5c5e89ad1f
Dave Markham <dashingdave@dashmac27.local>
2018-06-19 15:03:09 -0400

    My first commit.

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var myLabel: UILabel!
+	var count = 0
+	@IBAction func buttonTapped(_ sender: Any) {
+		count += 1
+		print(count)
+		if count >= 2 {
+			view.backgroundColor = UIColor.purple
+			myLabel.text = "#\(count) Dave is not here."
+			
+		}
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		// Change background color to red
+		view.backgroundColor = UIColor.green
+		
+		// The following code replaces what's in the ViewController, "This is a string."
+		myLabel.text = "Dave is here."
 	}
 
 	override func didReceiveMemoryWarning() {
